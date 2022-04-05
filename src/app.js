4196af6b1e05204193af5178c5eaa6e03bfccd4d
natigalsan <natividadgalsan@gmail.com>
2022-04-05 20:49:46 +0000

    Primer guardado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //ASIGNO mis dos variables, una con los iconos y otra con los números centrales:
+  const cards = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  //voyt a crear la función que accederá a los iconos y números de manera aleatoria:
+  function displayCards() {
+    let singleCard = cards[Math.floor(Math.random() * cards.length)];
+    let singleNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  }
 };
