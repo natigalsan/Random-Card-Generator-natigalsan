1413ba45972b6fa5bb9daa6b4eabf757b8eb6bb8
natigalsan <natividadgalsan@gmail.com>
2022-04-05 22:27:29 +0000

    tercer guardado

--- src/app.js
+++ src/app.js
@@ -6,31 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 import { Button } from "bootstrap";
 
-Button.addEvenListener("click", function() {
+window.displayCards = function displayCards() {
   //write your code here
   //ASIGNO mis dos variables, una con los iconos y otra con los números centrales:
   //voy a crear la función que accederá a los iconos y números de manera aleatoria:
-  function displayCards() {
-    const cards = ["♦", "♥", "♠", "♣"];
-    const numbers = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-    let singleCard = cards[Math.floor(Math.random() * cards.length)];
-    let singleNumbers = numbers[Math.floor(Math.random() * numbers.length)];
-    // restablecer el cuerpo del sitio web con la nueva salida html:
-    document.querySelector(".card").innerHTML = ` <div class="card">
+
+  const cards = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let singleCard = cards[Math.floor(Math.random() * cards.length)];
+  let singleNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  // restablecer el cuerpo del sitio web con la nueva salida html:
+  document.querySelector(".card").innerHTML = ` <div class="card">
     <div class="row" id="row1">
       <span class="item1">${singleCard}</span>
     </div>
@@ -41,5 +41,4 @@ Button.addEvenListener("click", function() {
       <span class="item2">${singleCard}</span>
     </div>
   </div>`;
-  }
-});
+};
